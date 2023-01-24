65488530a4a34ccebe9469316e29120cbeb1f355
Justin Monserrat <107101778+justinMonserrat@users.noreply.github.com>
2023-01-24 17:45:53 -0500

    done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Stores elements from html file
+//Stores elements from html file as variables
 const cards = Array.from(document.querySelectorAll('.gameCards'));
 const tLabel = document.getElementById("timeLabel");
 const tCounter = document.getElementById("timeCounter");
@@ -14,17 +14,18 @@ var firstCard;
 var secondCard;
 var noFlips = true;
 
-//When the page is loaded the restart button will be hidden and the cards will be shuffled
+//When the page is loaded the cards will be shuffled, locked, and
+//the restart button will be hidden so only the start button shows until the game starts
 window.onload = function() {
     rButton.style.display = "none";
     setCardsLock(true);
     shuffleCards();
 }
     
-//
+//Executed when the "startButton" is clicked
 function startGame() {
 
-    setCardsLock(false);
+    setCardsLock(false); //Unlocks cards
 
     //Removes the start button and then adds the restart button to the screen
     sButton.style.display = "none";
@@ -32,9 +33,7 @@ function startGame() {
     
     //Timer
     setInterval(function() {
-
-        time > 0 ? time-- : gameOver();
-
+        time > 0 ? time-- : gameOver(); //Ternary operator- conditional ? true : false
         tCounter.innerHTML = time; //Adds the timer to the screen
     }, 1000)
 }
@@ -44,13 +43,13 @@ function restartGame() {
     location.reload();
 }
 
-//This function is executed when the time runs out or the players makes all 6 matches
+//Executed when the time runs out or the player matches all 6 pairs
 function gameOver() {
     
-    setCardsLock(true);
-    clearInterval();
+    setCardsLock(true); //Locks cards
+    clearInterval(); //Clears timer interval
 
-    //Different displays based on if the user wins or fails
+    //Different text displays based on if the user wins or fails
     if (matches === 6) {
         tLabel.innerHTML = "You Win!";
         tCounter.style.display = "none";
@@ -64,7 +63,7 @@ function gameOver() {
         tLabel.style.margin = "1.5em";
     }
     
-    //Whichever cards the user did not match will flip over and reveal themselves a second after the game ends
+    //Whichever cards the user did not match will flip over being revealed a second after the game ends
     setTimeout(() => {
         cards.forEach(card => card.classList.add('flip'));
     }, 1000);
@@ -72,6 +71,7 @@ function gameOver() {
 
 //
 function shuffleCards() {
+    //forEach instead of for so each card is easily assigned a unique value
     cards.forEach(card => {
         let randomPosition = Math.floor(Math.random() * 12);
         card.style.order = randomPosition;
@@ -91,12 +91,14 @@ function setCardsLock(lockCards) {
     }
 }
 
-//
+//Executed if the card is unlocked and clicked
 function flipCard() {
-    if (!this.classList.contains('flip')) {
+    if (!this.classList.contains('flip')) { //Makes sure it's not alread flipped over
 
-        this.classList.add('flip');
+        this.classList.add('flip'); //Adds css flip animation
 
+        //Keeps track of if 1 or 2 cards are flipped over 
+        //and assigns the card data to the variable
         if (noFlips == true) {
             firstCard = this;
             noFlips = false;
@@ -105,21 +107,19 @@ function flipCard() {
             secondCard = this;
             noFlips = true;
         }
-
         checkCards();
     }
 }  
 
-//
+//Used to determine if the two cards flipped over are a match 
 function checkCards() {
 
-    if (firstCard != null && secondCard != null) {
+    if (firstCard != null && secondCard != null) { //Only checks when two cards have been flipped
 
-        setCardsLock(true);
+        setCardsLock(true); //Locks cards while checking
 
-        if (firstCard.getElementsByTagName("img")[0].src === secondCard.getElementsByTagName("img")[0].src) {
+        if (firstCard.getElementsByTagName("img")[0].src === secondCard.getElementsByTagName("img")[0].src) { //Checks if the images match
             matches++;
-
             mCounter.innerHTML = matches;
 
             firstCard = undefined;
@@ -128,9 +128,9 @@ function checkCards() {
             if (matches === 6) {
                 gameOver();
             } 
-            else setCardsLock(false);
+            else setCardsLock(false); //Unlocks Cards
         }
-        else {
+        else { //Flips the cards back over if they are not a match
             setTimeout(() => {
                firstCard.classList.remove('flip');
                secondCard.classList.remove('flip');
@@ -138,7 +138,7 @@ function checkCards() {
                firstCard = undefined;
                secondCard = undefined;
 
-               setCardsLock(false);
+               setCardsLock(false); //Unlocks cards
             }, 1750);
         }
     }
